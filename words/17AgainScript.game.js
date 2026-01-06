f8f04da90e9e92e929b18a0982dd82a300c0e75d
dennispr <dennispr@gmail.com>
2026-01-06 12:45:18 -0600

    redoing the movies

---
+++ words/17AgainScript.game.js
@@ -0,0 +1,421 @@
+// Auto-generated game module for 17AgainScript
+export const META = {
+  "title": "17AgainScript",
+  "author": "Unknown",
+  "aliases": []
+};
+export const WORD_COUNTS = {
+  "AGAIN": 6,
+  "SCRIPT": 11,
+  "IMSDB": 6,
+  "THE": 35,
+  "INTERNET": 1,
+  "MOVIE": 14,
+  "DATABASE": 1,
+  "WEB": 1,
+  "LARGEST": 1,
+  "RESOURCE": 1,
+  "SEARCH": 1,
+  "ALPHABETICAL": 1,
+  "GENRE": 1,
+  "ACTION": 1,
+  "ADVENTURE": 1,
+  "ANIMATION": 1,
+  "COMEDY": 3,
+  "CRIME": 1,
+  "DRAMA": 2,
+  "FAMILY": 1,
+  "FANTASY": 1,
+  "FILM": 3,
+  "NOIR": 1,
+  "HORROR": 1,
+  "MUSICAL": 2,
+  "MYSTERY": 1,
+  "ROMANCE": 2,
+  "SCI": 1,
+  "SHORT": 1,
+  "THRILLER": 1,
+  "WAR": 1,
+  "WESTERN": 1,
+  "TRANSCRIPTS": 1,
+  "FUTURAMA": 1,
+  "SEINFELD": 1,
+  "SOUTH": 1,
+  "PARK": 1,
+  "STARGATE": 1,
+  "LOST": 1,
+  "SPONSOR": 1,
+  "INTERNATIONAL": 1,
+  "FRENCH": 1,
+  "SCRIPTS": 1,
+  "LATEST": 1,
+  "COMMENTS": 2,
+  "PREDATOR": 1,
+  "MASTER": 1,
+  "AND": 9,
+  "COMMANDER": 1,
+  "WHITE": 1,
+  "CHRISTMAS": 1,
+  "FANTASTIC": 1,
+  "BEASTS": 1,
+  "CRIMES": 1,
+  "GRINDELWALD": 1,
+  "LEGEND": 1,
+  "OPINION": 1,
+  "NONE": 1,
+  "AVAILABLE": 2,
+  "RATING": 3,
+  "NOT": 4,
+  "AVERAGE": 1,
+  "USER": 2,
+  "OUT": 12,
+  "WRITERS": 1,
+  "JASON": 2,
+  "FILARDI": 2,
+  "GENRES": 1,
+  "DATE": 3,
+  "OCTOBER": 1,
+  "RELEASE": 1,
+  "APRIL": 1,
+  "READ": 2,
+  "FOR": 2,
+  "STINKYRAF": 1,
+  "SEEN": 4,
+  "THIS": 8,
+  "PLANE": 2,
+  "TOTALLY": 2,
+  "USELESS": 1,
+  "GUESS": 1,
+  "WAS": 5,
+  "FORCED": 1,
+  "WATCH": 3,
+  "PRESSURE": 1,
+  "EMILY": 1,
+  "FUNNY": 1,
+  "ZAC": 7,
+  "EFRON": 5,
+  "WON": 1,
+  "HEART": 1,
+  "ONE": 5,
+  "SHOWS": 1,
+  "THAT": 7,
+  "ACTUALLY": 1,
+  "CAN": 1,
+  "ACT": 1,
+  "LOVED": 3,
+  "DINA": 1,
+  "SADEK": 1,
+  "GOOD": 3,
+  "BUT": 3,
+  "ORIGINAL": 5,
+  "COURT": 2,
+  "SPEECH": 2,
+  "MUCH": 2,
+  "BETTER": 3,
+  "THAN": 2,
+  "GAVE": 1,
+  "WHICH": 1,
+  "SMALL": 1,
+  "VERSION": 3,
+  "THINK": 2,
+  "SCENE": 1,
+  "COULD": 1,
+  "HAVE": 4,
+  "BEEN": 1,
+  "WITH": 2,
+  "YOU": 4,
+  "REALLY": 2,
+  "GOT": 1,
+  "MILLI": 1,
+  "BIT": 1,
+  "WHERE": 1,
+  "LETTER": 1,
+  "CRIED": 2,
+  "CUTE": 1,
+  "WHEN": 1,
+  "MIKE": 2,
+  "GREW": 1,
+  "DID": 2,
+  "SOUND": 1,
+  "LIKE": 4,
+  "AFTER": 1,
+  "VERY": 3,
+  "ANNOYING": 1,
+  "HIGH": 1,
+  "SCHOOL": 1,
+  "CRAZE": 1,
+  "ALL": 3,
+  "ENTHUSED": 1,
+  "LEARN": 1,
+  "WOULD": 1,
+  "PLAYING": 1,
+  "YOUNG": 1,
+  "MATTHEW": 2,
+  "PERRY": 2,
+  "CHARACTER": 2,
+  "BECAUSE": 3,
+  "LOVE": 2,
+  "DIDNT": 1,
+  "FIT": 1,
+  "PLAY": 1,
+  "SAME": 1,
+  "PERSON": 1,
+  "HIM": 2,
+  "WATCHED": 1,
+  "ANYWAY": 1,
+  "THOUGH": 2,
+  "SLIGHTLY": 1,
+  "WORRIED": 1,
+  "WOULDN": 1,
+  "WELL": 5,
+  "WRITTEN": 2,
+  "ACTED": 1,
+  "LAUGHED": 1,
+  "OVERALL": 1,
+  "SADIA": 1,
+  "ABOUT": 2,
+  "REGRET": 1,
+  "LIFE": 1,
+  "SPEND": 1,
+  "WRONGLY": 1,
+  "WANT": 2,
+  "CHANGE": 1,
+  "THINGS": 1,
+  "EVEN": 1,
+  "BOY": 1,
+  "DONNEL": 1,
+  "ENJOYED": 1,
+  "LOT": 1,
+  "TIMES": 1,
+  "RAY": 1,
+  "BEST": 1,
+  "MOVIES": 1,
+  "UNTIL": 1,
+  "DONA": 1,
+  "OVER": 2,
+  "JUST": 2,
+  "LOOKED": 1,
+  "PERFECT": 1,
+  "SCENES": 1,
+  "COULDN": 1,
+  "TAKE": 1,
+  "EYES": 1,
+  "OFF": 1,
+  "MADE": 1,
+  "DIALOGUES": 1,
+  "GOING": 2,
+  "TIME": 1,
+  "SOON": 1,
+  "KAL": 1,
+  "GREAT": 1,
+  "BURR": 1,
+  "STEERS": 1,
+  "JOB": 1,
+  "DRAFT": 2,
+  "LACKING": 1,
+  "EDITED": 1,
+  "CAME": 1,
+  "GRADING": 1,
+  "SHOULD": 1,
+  "FINAL": 1,
+  "RAELEE": 1,
+  "GOD": 1,
+  "AMAZING": 1,
+  "NOW": 1,
+  "FEEL": 1,
+  "WAY": 2,
+  "IMAGINATION": 1,
+  "PUT": 1,
+  "DIFFERENT": 1,
+  "THEN": 1,
+  "ACTUAL": 1,
+  "ADD": 1,
+  "YOUR": 1,
+  "OWN": 1,
+  "COMMENT": 2,
+  "NAME": 1,
+  "MAIL": 1,
+  "CHOOSE": 1,
+  "EXCELLENT": 1,
+  "AWFUL": 1,
+  "INDEX": 1,
+  "SUBMIT": 1,
+  "LINK": 1,
+  "DISCLAIMER": 1,
+  "PRIVACY": 1,
+  "POLICY": 1,
+  "CONTACT": 1
+};
+export const COMMON_TOP20 = [
+  [
+    "MOVIE",
+    14
+  ],
+  [
+    "OUT",
+    12
+  ],
+  [
+    "SCRIPT",
+    11
+  ],
+  [
+    "ZAC",
+    7
+  ],
+  [
+    "AGAIN",
+    6
+  ],
+  [
+    "IMSDB",
+    6
+  ],
+  [
+    "EFRON",
+    5
+  ],
+  [
+    "ONE",
+    5
+  ],
+  [
+    "ORIGINAL",
+    5
+  ],
+  [
+    "WELL",
+    5
+  ],
+  [
+    "HAVE",
+    4
+  ],
+  [
+    "LIKE",
+    4
+  ],
+  [
+    "SEEN",
+    4
+  ],
+  [
+    "ALL",
+    3
+  ],
+  [
+    "BECAUSE",
+    3
+  ],
+  [
+    "BETTER",
+    3
+  ],
+  [
+    "COMEDY",
+    3
+  ],
+  [
+    "DATE",
+    3
+  ],
+  [
+    "FILM",
+    3
+  ],
+  [
+    "GOOD",
+    3
+  ]
+];
+export const UNCOMMON_TOP20 = [
+  [
+    "ABOUT",
+    2
+  ],
+  [
+    "AVAILABLE",
+    2
+  ],
+  [
+    "CHARACTER",
+    2
+  ],
+  [
+    "COMMENT",
+    2
+  ],
+  [
+    "COMMENTS",
+    2
+  ],
+  [
+    "COURT",
+    2
+  ],
+  [
+    "CRIED",
+    2
+  ],
+  [
+    "DID",
+    2
+  ],
+  [
+    "DRAFT",
+    2
+  ],
+  [
+    "DRAMA",
+    2
+  ],
+  [
+    "FILARDI",
+    2
+  ],
+  [
+    "GOING",
+    2
+  ],
+  [
+    "HIM",
+    2
+  ],
+  [
+    "JASON",
+    2
+  ],
+  [
+    "JUST",
+    2
+  ],
+  [
+    "LOVE",
+    2
+  ],
+  [
+    "MATTHEW",
+    2
+  ],
+  [
+    "MIKE",
+    2
+  ],
+  [
+    "MUCH",
+    2
+  ],
+  [
+    "MUSICAL",
+    2
+  ]
+];
+
+// Game-ready object
+export const BOOK_FOR_GAME = {
+  ...META,
+  commonTop: COMMON_TOP20,
+  uncommonTop: UNCOMMON_TOP20
+};
+
+export default BOOK_FOR_GAME;
---
+++ words/AvengersThe2012Script.game.js
@@ -0,0 +1,339 @@
+// Auto-generated game module for AvengersThe2012Script
+export const META = {
+  "title": "AvengersThe2012Script",
+  "author": "Unknown",
+  "aliases": []
+};
+export const WORD_COUNTS = {
+  "AVENGERS": 5,
+  "THE": 14,
+  "SCRIPT": 10,
+  "IMSDB": 6,
+  "INTERNET": 1,
+  "MOVIE": 4,
+  "DATABASE": 1,
+  "WEB": 1,
+  "LARGEST": 1,
+  "RESOURCE": 1,
+  "SEARCH": 1,
+  "ALPHABETICAL": 1,
+  "GENRE": 1,
+  "ACTION": 3,
+  "ADVENTURE": 2,
+  "ANIMATION": 1,
+  "COMEDY": 1,
+  "CRIME": 1,
+  "DRAMA": 1,
+  "FAMILY": 1,
+  "FANTASY": 1,
+  "FILM": 3,
+  "NOIR": 1,
+  "HORROR": 1,
+  "MUSICAL": 1,
+  "MYSTERY": 1,
+  "ROMANCE": 1,
+  "SCI": 2,
+  "SHORT": 1,
+  "THRILLER": 1,
+  "WAR": 1,
+  "WESTERN": 1,
+  "TRANSCRIPTS": 1,
+  "FUTURAMA": 1,
+  "SEINFELD": 1,
+  "SOUTH": 1,
+  "PARK": 1,
+  "STARGATE": 1,
+  "LOST": 1,
+  "SPONSOR": 1,
+  "INTERNATIONAL": 1,
+  "FRENCH": 1,
+  "SCRIPTS": 1,
+  "LATEST": 1,
+  "COMMENTS": 2,
+  "PREDATOR": 1,
+  "MASTER": 1,
+  "AND": 4,
+  "COMMANDER": 1,
+  "WHITE": 1,
+  "CHRISTMAS": 1,
+  "FANTASTIC": 1,
+  "BEASTS": 1,
+  "CRIMES": 1,
+  "GRINDELWALD": 1,
+  "LEGEND": 1,
+  "OPINION": 1,
+  "NONE": 1,
+  "AVAILABLE": 2,
+  "RATING": 3,
+  "NOT": 4,
+  "AVERAGE": 1,
+  "USER": 2,
+  "OUT": 5,
+  "WRITERS": 1,
+  "JOSS": 3,
+  "WHEDON": 4,
+  "GENRES": 1,
+  "RELEASE": 1,
+  "DATE": 1,
+  "MAY": 1,
+  "READ": 2,
+  "FOR": 2,
+  "ANONYMOUS": 2,
+  "EXCELLENT": 2,
+  "FROM": 1,
+  "THIS": 3,
+  "SEEMS": 2,
+  "MORE": 1,
+  "LESS": 1,
+  "FINAL": 1,
+  "ACCORDING": 1,
+  "ORIGINAL": 1,
+  "WRITTEN": 2,
+  "ZAK": 1,
+  "PENN": 1,
+  "WAS": 1,
+  "BAD": 1,
+  "THAT": 2,
+  "FORGOT": 1,
+  "EVER": 1,
+  "HAPPENED": 1,
+  "WROTE": 1,
+  "ENTIRELY": 1,
+  "HIS": 3,
+  "VISION": 1,
+  "JABULANI": 1,
+  "PONGOLANI": 1,
+  "GOOD": 2,
+  "DRIVEN": 1,
+  "BUT": 2,
+  "END": 1,
+  "ALL": 1,
+  "QUITE": 1,
+  "CONVINCING": 2,
+  "WHY": 1,
+  "SAVE": 1,
+  "LOKI": 2,
+  "SKIN": 1,
+  "VILLIAN": 1,
+  "JUST": 1,
+  "STEPS": 1,
+  "AWAY": 1,
+  "SCOT": 1,
+  "FREE": 1,
+  "THOUGH": 1,
+  "SENT": 1,
+  "BACK": 1,
+  "OTHER": 1,
+  "WORLD": 1,
+  "FACE": 1,
+  "MASKED": 1,
+  "ENOUGH": 1,
+  "SURE": 1,
+  "LIKE": 3,
+  "MIGHT": 1,
+  "FAKE": 1,
+  "DOESN": 1,
+  "REALLY": 1,
+  "THINK": 1,
+  "VERY": 1,
+  "FUNNY": 1,
+  "GREAT": 1,
+  "TIE": 1,
+  "TOGETHER": 1,
+  "DIFFERENT": 1,
+  "HEROES": 1,
+  "SAME": 1,
+  "UNIVERSE": 1,
+  "SETS": 1,
+  "NICELY": 1,
+  "NEXT": 1,
+  "FILMS": 1,
+  "ADD": 1,
+  "YOUR": 1,
+  "OWN": 1,
+  "COMMENT": 2,
+  "NAME": 1,
+  "MAIL": 1,
+  "CHOOSE": 1,
+  "AWFUL": 1,
+  "INDEX": 1,
+  "SUBMIT": 1,
+  "LINK": 1,
+  "DISCLAIMER": 1,
+  "PRIVACY": 1,
+  "POLICY": 1,
+  "CONTACT": 1
+};
+export const COMMON_TOP20 = [
+  [
+    "SCRIPT",
+    10
+  ],
+  [
+    "IMSDB",
+    6
+  ],
+  [
+    "AVENGERS",
+    5
+  ],
+  [
+    "OUT",
+    5
+  ],
+  [
+    "MOVIE",
+    4
+  ],
+  [
+    "WHEDON",
+    4
+  ],
+  [
+    "ACTION",
+    3
+  ],
+  [
+    "FILM",
+    3
+  ],
+  [
+    "JOSS",
+    3
+  ],
+  [
+    "LIKE",
+    3
+  ],
+  [
+    "RATING",
+    3
+  ],
+  [
+    "ADVENTURE",
+    2
+  ],
+  [
+    "ANONYMOUS",
+    2
+  ],
+  [
+    "AVAILABLE",
+    2
+  ],
+  [
+    "COMMENT",
+    2
+  ],
+  [
+    "COMMENTS",
+    2
+  ],
+  [
+    "CONVINCING",
+    2
+  ],
+  [
+    "EXCELLENT",
+    2
+  ],
+  [
+    "GOOD",
+    2
+  ],
+  [
+    "LOKI",
+    2
+  ]
+];
+export const UNCOMMON_TOP20 = [
+  [
+    "ADVENTURE",
+    2
+  ],
+  [
+    "ANONYMOUS",
+    2
+  ],
+  [
+    "AVAILABLE",
+    2
+  ],
+  [
+    "COMMENT",
+    2
+  ],
+  [
+    "COMMENTS",
+    2
+  ],
+  [
+    "CONVINCING",
+    2
+  ],
+  [
+    "EXCELLENT",
+    2
+  ],
+  [
+    "GOOD",
+    2
+  ],
+  [
+    "LOKI",
+    2
+  ],
+  [
+    "READ",
+    2
+  ],
+  [
+    "SCI",
+    2
+  ],
+  [
+    "SEEMS",
+    2
+  ],
+  [
+    "USER",
+    2
+  ],
+  [
+    "WRITTEN",
+    2
+  ],
+  [
+    "ACTION",
+    3
+  ],
+  [
+    "FILM",
+    3
+  ],
+  [
+    "JOSS",
+    3
+  ],
+  [
+    "LIKE",
+    3
+  ],
+  [
+    "RATING",
+    3
+  ],
+  [
+    "MOVIE",
+    4
+  ]
+];
+
+// Game-ready object
+export const BOOK_FOR_GAME = {
+  ...META,
+  commonTop: COMMON_TOP20,
+  uncommonTop: UNCOMMON_TOP20
+};
+
+export default BOOK_FOR_GAME;
